f9257eeba7a4e820e08d7d44fda7962a2190b5e2
alxb421 <alb421@gmail.com>
2021-09-24 16:56:31 +0300

    fixed wrong commit

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#   Copyright (c) 2019-2021 Alexandru Balan under the PearProject. All rights reserved
+
+
 if (whiptail --title "pearOS icon pack installation wizard" --yesno "Do you want to install the default pearOS icon pack? This will include the normal and dark mode version." 8 78); then
     sudo mv ./* /usr/share/icons
 else
